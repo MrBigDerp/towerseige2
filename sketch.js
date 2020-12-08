e172004a19d986c5f2da0a7536ab8cabad9a32f8
MrBigDerp <73116185+MrBigDerp@users.noreply.github.com>
2020-12-08 08:49:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,91 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var platform, block1;
+var slingshot, polyObj;
+
+function preload(){
+
+}
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    block1 = new Block(580,290,60,40);
+    block2 = new Block(640,290,60,40);
+    block3 = new Block(700,290,60,40);
+    block4 = new Block(760,290,60,40);
+    block5 = new Block(820,290,60,40);
+
+    block6 = new Block(640,250,60,40);
+    block7 = new Block(700,250,60,40);
+    block8 = new Block(760,250,60,40);
+
+    block9 = new Block(700,210,60,40);
+
+    block10 = new Block(999,145,66,40)
+    block11 = new Block(1065,145,66,40)
+    block12 = new Block(1131,145,66,40)
+
+    block13 = new Block(1065,105,66,40)
+
+    platform = new Ground(700,300,305,10)
+    platform1 = new Ground(1065,155,203,10)
+    grnd = new Ground(600,600,1200,20)
+
+    polyObj = new Poly(50,300,130);
+
+    //slingshot = new SlingShot(polyObj.body,{x:100, y:200});
+
+}
+function draw(){
+    background("black");
+    Engine.update(engine);
+    strokeWeight(4);
+    text(mouseX+","+mouseY,mouseX,mouseY);
+
+    fill("orange")
+    block1.display();
+    block2.display();
+    block3.display();
+    block4.display();
+    block5.display();
+    fill("purple")
+    block6.display();
+    block7.display();
+    block8.display();
+    block10.display();
+    block11.display();
+    block12.display();
+    fill("blue")
+    block13.display();
+    block9.display();
+    platform.display();
+    platform1.display();
+    polyObj.display();
+    slingshot.display();
+    //console.log(slingshot);
+    grnd.display();
+
+}
+
+function keyPressed(){
+    if(keyCode===32){
+        Matter.Body.setPosition(polyObj.body,{x:50,y:300});
+        slingshot.attach(polyObj.body);
+    }
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(polyObj.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+}
